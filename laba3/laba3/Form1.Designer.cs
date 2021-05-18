6c9f5fe62cdef23cb433eabde4c94a4de0ea6370
Vladyslav <58324552+LaBetForLad@users.noreply.github.com>
2021-05-18 12:04:16 +0300

    Add files via upload

---
+++ laba3/laba3/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.time = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.text_Ax = new System.Windows.Forms.TextBox();
+            this.text_Bx = new System.Windows.Forms.TextBox();
+            this.text_Cx = new System.Windows.Forms.TextBox();
+            this.text_Ay = new System.Windows.Forms.TextBox();
+            this.text_By = new System.Windows.Forms.TextBox();
+            this.text_Cy = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.text_Dx = new System.Windows.Forms.TextBox();
+            this.text_Dy = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Location = new System.Drawing.Point(12, 9);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(0, 13);
+            this.time.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(817, 339);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 367);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "кординати точок трикутника";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "точка A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 455);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "точка B:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 496);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "точка C:";
+            // 
+            // text_Ax
+            // 
+            this.text_Ax.Location = new System.Drawing.Point(85, 405);
+            this.text_Ax.Name = "text_Ax";
+            this.text_Ax.Size = new System.Drawing.Size(68, 20);
+            this.text_Ax.TabIndex = 6;
+            // 
+            // text_Bx
+            // 
+            this.text_Bx.Location = new System.Drawing.Point(85, 448);
+            this.text_Bx.Name = "text_Bx";
+            this.text_Bx.Size = new System.Drawing.Size(68, 20);
+            this.text_Bx.TabIndex = 7;
+            // 
+            // text_Cx
+            // 
+            this.text_Cx.Location = new System.Drawing.Point(85, 489);
+            this.text_Cx.Name = "text_Cx";
+            this.text_Cx.Size = new System.Drawing.Size(68, 20);
+            this.text_Cx.TabIndex = 8;
+            // 
+            // text_Ay
+            // 
+            this.text_Ay.Location = new System.Drawing.Point(169, 405);
+            this.text_Ay.Name = "text_Ay";
+            this.text_Ay.Size = new System.Drawing.Size(68, 20);
+            this.text_Ay.TabIndex = 9;
+            // 
+            // text_By
+            // 
+            this.text_By.Location = new System.Drawing.Point(169, 448);
+            this.text_By.Name = "text_By";
+            this.text_By.Size = new System.Drawing.Size(68, 20);
+            this.text_By.TabIndex = 10;
+            // 
+            // text_Cy
+            // 
+            this.text_Cy.Location = new System.Drawing.Point(169, 489);
+            this.text_Cy.Name = "text_Cy";
+            this.text_Cy.Size = new System.Drawing.Size(68, 20);
+            this.text_Cy.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 389);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(194, 389);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(343, 389);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Введіть кординати точки";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(325, 427);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "X";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(325, 455);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Y";
+            // 
+            // text_Dx
+            // 
+            this.text_Dx.Location = new System.Drawing.Point(365, 421);
+            this.text_Dx.Name = "text_Dx";
+            this.text_Dx.Size = new System.Drawing.Size(68, 20);
+            this.text_Dx.TabIndex = 17;
+            // 
+            // text_Dy
+            // 
+            this.text_Dy.Location = new System.Drawing.Point(365, 455);
+            this.text_Dy.Name = "text_Dy";
+            this.text_Dy.Size = new System.Drawing.Size(68, 20);
+            this.text_Dy.TabIndex = 18;
+            this.text_Dy.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(301, 526);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Лежить:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Перевірити точку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(526, 410);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 30);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Змінити колір";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(27, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 22;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(124, 526);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Намалювати ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(853, 577);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.text_Dy);
+            this.Controls.Add(this.text_Dx);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.text_Cy);
+            this.Controls.Add(this.text_By);
+            this.Controls.Add(this.text_Ay);
+            this.Controls.Add(this.text_Cx);
+            this.Controls.Add(this.text_Bx);
+            this.Controls.Add(this.text_Ax);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.time);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text_Ax;
+        private System.Windows.Forms.TextBox text_Bx;
+        private System.Windows.Forms.TextBox text_Cx;
+        private System.Windows.Forms.TextBox text_Ay;
+        private System.Windows.Forms.TextBox text_By;
+        private System.Windows.Forms.TextBox text_Cy;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox text_Dx;
+        private System.Windows.Forms.TextBox text_Dy;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ laba3/laba3/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private Graphics graphics;
+        private Stopwatch stopwatch = new Stopwatch();
+
+        PointF A;
+        PointF B;
+        PointF C;
+        PointF D;
+
+
+        public void AlgorithmDrawingOfRectangle(int ax, int ay, int bx, int by, int cx, int cy, PictureBox picture)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+            graphics = picture.CreateGraphics();
+
+            A = new PointF(ax,ay);
+            B = new PointF(bx,by);
+            C = new PointF(cx,cy);
+
+
+            float x1, x2;
+          
+
+            for (float sy = A.Y; sy < C.Y; ++sy)
+            {
+                x1 = A.X + (sy - A.Y) * (C.X - A.X) / (C.Y - A.Y);
+
+                if (sy < B.Y)
+                {
+                    x2 = A.X + (sy - A.Y) * (B.X - A.X) / (B.Y - A.Y);
+                }
+                else
+                {
+                    if (C.Y == B.Y)
+                    {
+                        x2 = B.X;
+                    }
+                    else
+                    {
+                        x2 = B.X + (sy - B.Y) * (C.X - B.X) / (C.Y - B.Y);
+                    }
+
+                }
+
+                if (x1 > x2)
+                {
+                    float tmp = x1;
+                    x1 = x2; x2 = tmp;
+                }
+
+                graphics.DrawLine(new Pen(Color.Green), x1, sy, x2, sy);
+            }
+
+            stopwatch.Stop();
+            label11.Text = "Час: " + stopwatch.Elapsed.TotalMilliseconds.ToString();
+        }
+
+
+        public void AlgorithmDrawingAnoutherCollorOf(int ax, int ay, int bx, int by, int cx, int cy, PictureBox picture,Pen p)
+        {
+            graphics = picture.CreateGraphics();
+
+            A = new PointF(ax, ay);
+            B = new PointF(bx, by);
+            C = new PointF(cx, cy);
+
+            
+           
+            float x1, x2;
+            
+            for (float sy = A.Y; sy < C.Y; ++sy)
+            {
+                x1 = A.X + (sy - A.Y) * (C.X - A.X) / (C.Y - A.Y);
+
+                if (sy < B.Y)
+                {
+                    x2 = A.X + (sy - A.Y) * (B.X - A.X) / (B.Y - A.Y);
+                }
+                else
+                {
+                    if (C.Y == B.Y)
+                    {
+                        x2 = B.X;
+                    }
+                    else
+                    {
+                        x2 = B.X + (sy - B.Y) * (C.X - B.X) / (C.Y - B.Y);
+                    }
+
+                }
+
+                if (x1 > x2)
+                {
+                    float tmp = x1;
+                    x1 = x2; x2 = tmp;
+                }
+           
+                graphics.DrawLine(p, x1, sy, x2, sy);
+            }
+        }
+
+
+        public void SearchPoint(int dx,int dy)
+        {
+            D = new PointF(dx,dy);
+           
+
+
+            if (D.X > 996 || D.X < 0 || D.Y > 426 || D.Y < 0)
+            {
+                MessageBox.Show("Точка не може знаходиться за межами границь!");
+            }
+            else
+            {
+                bool res = false;
+
+
+                float a = (A.X - D.X) * (B.Y - A.Y) - (B.X - A.X) * (A.Y - D.Y);
+                float b = (B.X - D.X) * (C.Y - B.Y) - (C.X - B.X) * (B.Y - D.Y);
+                float c = (C.X - D.X) * (A.Y - C.Y) - (A.X - C.X) * (C.Y - D.Y);
+
+                if ((a <= 0 && b <= 0 && c <= 0) || (a >= 0 && b >= 0 && c >= 0))
+                {
+                    res = true;
+                }
+
+
+                if (res == true)
+                {
+                    label10.Text = "true";
+                }
+                else
+                {
+                    label10.Text = "false";
+                }
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(text_Ax.Text) == true || 
+                string.IsNullOrEmpty(text_Ay.Text) || 
+                string.IsNullOrEmpty(text_Bx.Text) ||
+                string.IsNullOrEmpty(text_By.Text) ||
+                string.IsNullOrEmpty(text_Cx.Text) ||
+                string.IsNullOrEmpty(text_Cy.Text))               
+            {
+                MessageBox.Show("Ви не ввели координати!");
+            }
+
+            else
+            {
+                AlgorithmDrawingOfRectangle(int.Parse(text_Ax.Text), int.Parse(text_Ay.Text),
+                                          int.Parse(text_Bx.Text), int.Parse(text_By.Text),
+                                          int.Parse(text_Cx.Text), int.Parse(text_Cy.Text), pictureBox1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (new Random().Next(1, 4))
+            {
+
+                case 1:
+                    AlgorithmDrawingAnoutherCollorOf(int.Parse(text_Ax.Text), int.Parse(text_Ay.Text),
+                                         int.Parse(text_Bx.Text), int.Parse(text_By.Text),
+                                          int.Parse(text_Cx.Text), int.Parse(text_Cy.Text), pictureBox1, new Pen(Color.Red));
+                    break;
+
+                case 2:
+                    AlgorithmDrawingAnoutherCollorOf(int.Parse(text_Ax.Text), int.Parse(text_Ay.Text),
+                                      int.Parse(text_Bx.Text), int.Parse(text_By.Text),
+                                       int.Parse(text_Cx.Text), int.Parse(text_Cy.Text), pictureBox1, new Pen(Color.Yellow));
+                    break;
+
+                case 3:
+                    AlgorithmDrawingAnoutherCollorOf(int.Parse(text_Ax.Text), int.Parse(text_Ay.Text),
+                                       int.Parse(text_Bx.Text), int.Parse(text_By.Text),
+                                        int.Parse(text_Cx.Text), int.Parse(text_Cy.Text), pictureBox1, new Pen(Color.Black));
+                    break;
+            }
+      
+        }
+
+
+
+
+
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(text_Dx.Text) == true ||
+                string.IsNullOrEmpty(text_Dy.Text))
+            {
+                MessageBox.Show("Ви не ввели координати!");
+            }
+            else
+            {
+                SearchPoint(int.Parse(text_Dx.Text), int.Parse(text_Dy.Text));
+            }
+        }
+    }
+}
