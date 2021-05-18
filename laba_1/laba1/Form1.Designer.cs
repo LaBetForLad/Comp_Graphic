a6d244d298032b599c429994aefd8b2d91d410f2
Vladyslav <58324552+LaBetForLad@users.noreply.github.com>
2021-05-18 11:59:36 +0300

    Add files via upload

---
+++ laba_1/laba1/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.my = new System.Windows.Forms.Label();
+            this.CDA = new System.Windows.Forms.Label();
+            this.Brez = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.x1_text = new System.Windows.Forms.TextBox();
+            this.y1_text = new System.Windows.Forms.TextBox();
+            this.x2_text = new System.Windows.Forms.TextBox();
+            this.y2_text = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(462, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 242);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Алгоритм  Брезенхема";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Алгоритм Цифровий диференціальний аналізатор";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(508, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Алгоритм Мови програмування";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(360, 242);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 327);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(360, 242);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // my
+            // 
+            this.my.AutoSize = true;
+            this.my.Location = new System.Drawing.Point(684, 9);
+            this.my.Name = "my";
+            this.my.Size = new System.Drawing.Size(0, 13);
+            this.my.TabIndex = 16;
+            // 
+            // CDA
+            // 
+            this.CDA.AutoSize = true;
+            this.CDA.Location = new System.Drawing.Point(322, 9);
+            this.CDA.Name = "CDA";
+            this.CDA.Size = new System.Drawing.Size(0, 13);
+            this.CDA.TabIndex = 18;
+            // 
+            // Brez
+            // 
+            this.Brez.AutoSize = true;
+            this.Brez.Location = new System.Drawing.Point(204, 300);
+            this.Brez.Name = "Brez";
+            this.Brez.Size = new System.Drawing.Size(0, 13);
+            this.Brez.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(730, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Намальювати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // x1_text
+            // 
+            this.x1_text.Location = new System.Drawing.Point(434, 397);
+            this.x1_text.Name = "x1_text";
+            this.x1_text.Size = new System.Drawing.Size(100, 20);
+            this.x1_text.TabIndex = 23;
+            // 
+            // y1_text
+            // 
+            this.y1_text.Location = new System.Drawing.Point(434, 479);
+            this.y1_text.Name = "y1_text";
+            this.y1_text.Size = new System.Drawing.Size(100, 20);
+            this.y1_text.TabIndex = 25;
+            // 
+            // x2_text
+            // 
+            this.x2_text.Location = new System.Drawing.Point(597, 397);
+            this.x2_text.Name = "x2_text";
+            this.x2_text.Size = new System.Drawing.Size(100, 20);
+            this.x2_text.TabIndex = 27;
+            // 
+            // y2_text
+            // 
+            this.y2_text.Location = new System.Drawing.Point(597, 479);
+            this.y2_text.Name = "y2_text";
+            this.y2_text.Size = new System.Drawing.Size(100, 20);
+            this.y2_text.TabIndex = 29;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(459, 368);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "x1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(459, 452);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "y1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(622, 368);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 13);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "x2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(622, 452);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 13);
+            this.label10.TabIndex = 33;
+            this.label10.Text = "y2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 603);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.y2_text);
+            this.Controls.Add(this.x2_text);
+            this.Controls.Add(this.y1_text);
+            this.Controls.Add(this.x1_text);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Brez);
+            this.Controls.Add(this.CDA);
+            this.Controls.Add(this.my);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label my;
+        private System.Windows.Forms.Label CDA;
+        private System.Windows.Forms.Label Brez;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox x1_text;
+        private System.Windows.Forms.TextBox y1_text;
+        private System.Windows.Forms.TextBox x2_text;
+        private System.Windows.Forms.TextBox y2_text;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ laba_1/laba1/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba1
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        private Graphics graphics;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void AlgorithmCDADrawLine(int x1,int y1,int x2,int y2, PictureBox b)
+        {
+            graphics = b.CreateGraphics();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            int l;
+
+            if (Math.Abs(x2 - x1) >= Math.Abs(y2 - y1))
+            {
+                l = Math.Abs(x2 - x1);
+            }
+            else
+            {
+                l = Math.Abs(y2 - y1);
+            }
+
+            int del_x = 1;
+            int del_y = 1;
+
+            double x = x1 + 0.5 * Math.Sign(del_x);
+            double y = y1 + 0.5 * Math.Sign(del_y);
+
+            int i = 1;
+
+            while (i <= l)
+            {
+                graphics.FillRectangle(new SolidBrush(Color.Blue), Convert.ToInt32(x), Convert.ToInt32(y), 1, 1);
+                x += del_x;
+                y += del_y;
+                i++;
+            }
+            stopwatch.Stop();
+            CDA.Text = "Час: " + stopwatch.Elapsed.TotalMilliseconds.ToString();
+        }
+
+
+
+        public void BresenhamDrawLine(int x1, int y1, int x2, int y2, PictureBox b)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            graphics = b.CreateGraphics();
+
+            int x = x1;
+            int y = y1;
+
+            int p1 = Math.Abs(x2 - x1);
+            int p2 = Math.Abs(y2 - y1);
+
+            int s1 = Math.Sign(x2 - x1);
+            int s2 = Math.Sign(x2 - x1);
+
+            int e = 2 * p2 - p1;
+
+            bool flag = false;
+
+            if (p2 > p1)
+            {
+                int temp = p1;
+                p1 = p2;
+                p2 = temp;
+                flag = true;
+            }
+
+            for (int i = 0; i <= p1; ++i)
+            {
+                while (e >= 0)
+                {
+                    if (flag)
+                    {
+                        x += s1;
+                    }
+                    else
+                    {
+                        y += s2;
+                    }
+                    e -= 2 * p1;
+                }
+                if (flag)
+                {
+                    y += s2;
+                }
+                else
+                {
+                    x += s1;
+                }
+                e += 2 * p2;
+                graphics.FillRectangle(new SolidBrush(Color.Green), x, y, 1, 1);
+            }
+
+            stopwatch.Stop();
+            Brez.Text = "Час: " + stopwatch.Elapsed.TotalMilliseconds.ToString();
+        }
+
+
+        public void LibraryDrawLine(int x1, int y1, int x2, int y2, PictureBox b)
+        {
+
+            graphics = b.CreateGraphics();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            Pen pen = new Pen(Color.Blue);
+            graphics.DrawLine(pen, x1, y1, x2, y2);
+
+            stopwatch.Stop();
+            my.Text = "Час: " + stopwatch.Elapsed.TotalMilliseconds.ToString();
+        }
+
+
+
+
+
+
+
+
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(x1_text.Text) == true || string.IsNullOrEmpty(y1_text.Text) || string.IsNullOrEmpty(x2_text.Text) || string.IsNullOrEmpty(y2_text.Text))
+            {
+                MessageBox.Show("Ви не ввели координати!");
+            }
+            else
+            {
+                BresenhamDrawLine(int.Parse(x1_text.Text), int.Parse(y1_text.Text), int.Parse(x2_text.Text), int.Parse(y2_text.Text), pictureBox3);
+                AlgorithmCDADrawLine(int.Parse(x1_text.Text), int.Parse(y1_text.Text), int.Parse(x2_text.Text), int.Parse(y2_text.Text), pictureBox2);
+                LibraryDrawLine(int.Parse(x1_text.Text), int.Parse(y1_text.Text), int.Parse(x2_text.Text), int.Parse(y2_text.Text), pictureBox1);
+            }
+
+
+
+        }
+    }
+}
